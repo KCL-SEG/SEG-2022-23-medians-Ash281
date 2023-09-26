a4a219582cacdb0464a5b35a5844f3e4f5d31fd6
Ash281 <ashvin.valentine281@gmail.com>
2023-09-26 13:28:29 +0100

    medians

--- median.py
+++ median.py
@@ -1,15 +1,12 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+import numpy
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        arrLength = len(numbers)
-        if arrLength % 2 == 0:
-            median = (numbers[arrLength//2] + numbers[(arrLength-1)//2]) / 2
-        else:
-            median = numbers[arrLength//2]
+        median = numpy.median(numbers)
         print(median)
     except ValueError:
         print("Some input could not be converted to a number!")
