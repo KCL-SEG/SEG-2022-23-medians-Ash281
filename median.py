f7796cdcac27cfb57189974c2f997190d30dd0ed
Ash281 <ashvin.valentine281@gmail.com>
2023-09-26 13:02:21 +0100

    medians

--- median.py
+++ median.py
@@ -3,14 +3,14 @@
 
 numbers = [float(value) for value in input().split(",")]
 arrLength = len(numbers)
-if (arrLength % 2 == 1){
+if (arrLength % 2 == 1):
     median = numbers[(arrLength-1)/2]
     print(median)
-}
-else {
+
+else:
     median = numbers[arrLength/2] + numbers[(arrLength-2)/2] 
     print(median)
-}
+
 
  
 
