7012b9ad77bb74665a1f879fa0b30e92c122d3f5
Ash281 <ashvin.valentine281@gmail.com>
2023-09-26 11:29:32 +0100

    medians

--- median.py
+++ median.py
@@ -5,8 +5,14 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        arrLength = len(numbers)
+        median = (numbers[arrLength/2] + numbers[(arrLength-1)/2])/2
+        print(median)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+    
+    
+
